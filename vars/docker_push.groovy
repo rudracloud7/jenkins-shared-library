f3f1fdd058b2ebcc47b2cd08c7a386a493b032c7
rudracloud7 <rathorrudra268@gmail.com>
2026-03-15 16:58:25 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,13 @@
+def call(){String credsId, String imageName){
+  withCredentials([usernamePassword(
+                    credentialsId:"${credId}",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser",
+                )]){
+                    
+                sh "docker login -u $dockerHubUser -p $dockerHubPass"
+                sh "docker image tag "$(imageName)" $dockerHubUser/${imageName}"
+                sh "docker push $dockerHubUser/${imageName}:latest"
+            
+                }
+}
